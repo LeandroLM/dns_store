c80af9c4a8858ef1531a4373a4a2050d6dee1fa4
Leandro Marques <leandrolmarques@gmail.com>
2019-06-05 08:08:15 -0300

    Add specs for list endpoint

--- app/controllers/api/dns_controller.rb
+++ app/controllers/api/dns_controller.rb
@@ -8,6 +8,22 @@ module Api
       end
     end
 
+    def list
+      render json: {
+        data: {
+          dns_records_count: 2,
+          dns_records: [
+            DnsRecord.find_by(ip_address: '1.1.1.1'),
+            DnsRecord.find_by(ip_address: '3.3.3.3')
+          ],
+          hostnames: [
+            { hostname: 'lorem.com', matching_dns_records: 1 },
+            { hostname: 'amet.com', matching_dns_records: 2 }
+          ]
+        }
+      }
+    end
+
     private
 
     def dns_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :dns, only: [:create]
+    resources :dns, only: :create do
+      post :list, on: :collection
+    end
   end
 end
